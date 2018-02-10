9004ee2e568c868ce3ccddeef9564fbbd2e4f43f
DenRebrov <den_rebrov@list.ru>
2018-02-10 16:39:38 +0300

    add design table

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,6 +23,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: [:password, :password_confirmation, :current_password])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :birthday])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :birthday, :ava])
   end
 end
