eb35b9bbce9620a1da1c4005e4facb9928698bfd
DenRebrov <den_rebrov@list.ru>
2018-02-14 15:26:37 +0300

    test pg 2

--- db/migrate/20180213080608_create_comments.rb
+++ db/migrate/20180213080608_create_comments.rb
@@ -3,7 +3,8 @@ class CreateComments < ActiveRecord::Migration[5.1]
     create_table :comments do |t|
       t.text :body
       t.integer :user_id
-      t.references :commentable, polymorphic: true
+      t.integer :commentable_id
+      t.string :commentable_type
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -15,11 +15,10 @@ ActiveRecord::Schema.define(version: 20180213080608) do
   create_table "comments", force: :cascade do |t|
     t.text "body"
     t.integer "user_id"
-    t.string "commentable_type"
     t.integer "commentable_id"
+    t.string "commentable_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id"
   end
 
   create_table "targets", force: :cascade do |t|
