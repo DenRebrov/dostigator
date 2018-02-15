9e5e6636c29102001320640e72961926d840819f
DenRebrov <den_rebrov@list.ru>
2018-02-15 11:09:48 +0300

    add commentable ID

--- db/migrate/20180215070622_add_commentable_to_comments.rb
+++ db/migrate/20180215070622_add_commentable_to_comments.rb
@@ -1,5 +1,5 @@
 class AddCommentableToComments < ActiveRecord::Migration[5.1]
   def change
-    add_reference :comments, :commentable, polymorphic: true
+    add_reference :comments, :commentable, polymorphic: true, index: true
   end
 end
