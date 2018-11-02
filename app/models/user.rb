5de41cab7296834e3a68bdca15f3693361547b00
DenRebrov <den_rebrov@list.ru>
2018-11-02 16:04:52 +0300

    deign users#show for mobile

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,9 @@ class User < ApplicationRecord
   has_many :comments, as: :commentable, dependent: :destroy
   has_many :targets, dependent: :destroy
 
-  validates :name, presence: true, length: {maximum: 20}
+  validates :name, presence: true, length: {maximum: 16}
   validates :name, uniqueness: true
 
   # Добавляем аплоадер аватарок, чтобы заработал carrierwave
   mount_uploader :ava, AvaUploader
 end
-
-
